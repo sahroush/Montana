6ca5d244d5364c08f41a0d31b73e2f019399a424
Parsa Alizadeh <33874579+ParsaAlizadeh@users.noreply.github.com>
2020-08-15 15:33:46 +0430

    Reformat code (#6)
    * Reformat files
    
    * Get token from env
    
    * Nonlocal variable `cur`
    
    * Check reacted message
    
    * Deleted message handler

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 # bot.py
+
+import random
+import textwrap
+import time
 import os
 
-import textwrap 
 import discord
+import requests
 from discord.ext import commands
-from dotenv import load_dotenv
-import time, requests, random
 
-load_dotenv()
-TOKEN = "Njk5NTk5MzkwNDMxNzcyNzMz" + ".XpWutA.0nvimxpX" + "AW7uNlwRD" + "wW1aok8Zvw"
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    TOKEN = "Njk5NTk5MzkwNDMxNzcyNzMz" + ".XpWutA.0nvimxpX" + "AW7uNlwRD" + "wW1aok8Zvw"    # The most idiotic idea
 
 bot = commands.Bot(command_prefix='`')
 
@@ -31,117 +34,125 @@ async def on_member_join(member):
 def makeUrl(afterID, subreddit):
     return subreddit.split('/.json')[0] + "/.json?after={}".format(afterID)
 
+
 def ismedia(imageUrl):
-    return(('.jpg' in imageUrl or '.webm' in imageUrl or \
-    '.gif' in imageUrl or '.png' in imageUrl) and not '.gifv' in imageUrl)
+    return ('.jpg' in imageUrl or
+            '.webm' in imageUrl or
+            '.gif' in imageUrl or
+            '.png' in imageUrl) and not '.gifv' in imageUrl
+
 
 def fetch(sub):
-    url = makeUrl('',"https://www.reddit.com/r/"+sub)
+    url = makeUrl('', "https://www.reddit.com/r/" + sub)
     subJson = requests.get(url, headers={'User-Agent': 'Montana'}).json()
     sfw = []
     nsfw = []
-    try : 
+    try:
         posts = subJson['data']['children']
         for post in posts:
             ismed = ismedia(post['data']['url'])
-            if(ismed and post['data']['over_18']):
-                nsfw += [[post['data']['title'] , post['data']['url']]]
-            elif(ismed):
-                sfw  += [[post['data']['title'] , post['data']['url']]]
-    except :
+            if ismed and post['data']['over_18']:
+                nsfw += [[post['data']['title'], post['data']['url']]]
+            elif ismed:
+                sfw += [[post['data']['title'], post['data']['url']]]
+    except:
         pass
-    return(sfw , nsfw)
+    return sfw, nsfw
 
 
-@bot.command(name='echo', help='Repeats a given message' , usage = "[message...]")
-async def echo(ctx , *response):
-    if (len(response) == 0):
+@bot.command(name='echo', help='Repeats a given message', usage="[message...]")
+async def echo(ctx, *response):
+    if not response:
         response = ["**I can't send an empty message you fucking idiot**"]
     await ctx.send(" ".join(response))
 
+
 def wrapped(s):
-    wrapper = textwrap.TextWrapper(width=45) 
+    wrapper = textwrap.TextWrapper(width=45)
     word_list = wrapper.wrap(text=s)
     s = ""
     for word in word_list:
         s += '\n' + word
-    return(s)
+    return s
 
-@bot.command(name='album', help='posts the most recent pics from the given subreddit \n'+
-'nsfw is off in sfw channels unless +nsfw is used \n'+
-'shuffles posts when +random is used ' ,usage = "<subreddit> [+nsfw][+random]")
-async def album(ctx ,sub , *args):
-    sfw , nsfw = fetch(sub)
+
+@bot.command(name='album', help='posts the most recent pics from the given subreddit \n' +
+                                'nsfw is off in sfw channels unless +nsfw is used \n' +
+                                'shuffles posts when +random is used ', usage="<subreddit> [+nsfw][+random]")
+async def album(ctx, sub, *args):
+    sfw, nsfw = fetch(sub)
     posts = sfw
-    if("+nsfw" in args or ctx.channel.is_nsfw()):
+    if "+nsfw" in args or ctx.channel.is_nsfw():
         posts += nsfw
-    if(len(posts) == 0):
+    if not posts:
         response = "Sorry, couldn't find a pic :sob:"
         await ctx.send(response)
         return
-    if("+random" in args):
+    if "+random" in args:
         random.shuffle(posts)
     cur = 0
-    sub = "https://www.reddit.com/r/"+sub
-    embed=discord.Embed(title=wrapped(posts[cur][0]), description="", color=242424 , url = posts[cur][1])
-    embed.set_footer(text=str(cur+1)+"/"+str(len(posts)))
+    sub = "https://www.reddit.com/r/" + sub
+    embed = discord.Embed(title=wrapped(posts[cur][0]), description="", color=242424, url=posts[cur][1])
+    embed.set_footer(text=str(cur + 1) + "/" + str(len(posts)))
     embed.set_image(url=posts[cur][1])
-    
-    message = await ctx.send(embed = embed);
-    
-    emojis = ["⏪" , "⏩" , "🗑"]
-    
+
+    message = await ctx.send(embed=embed)
+
+    emojis = ["⏪", "⏩", "🗑"]
+
     def check(reaction, user):
-        if(user == message.author): #the reaction was made by the bot itself
-            return(False)
-        return(True) #made by user || third party
-    
+        if user == message.author or reaction.message.id != message.id:  # the reaction was made by the bot itself
+            return False
+        return True  # made by user || third party
+
     async def Check(reaction, user):
         await message.remove_reaction(reaction, user)
-        return not(user != ctx.author or not(str(reaction) in emojis))
-        
+        return not (user != ctx.author or not (str(reaction) in emojis))
+
     await message.clear_reactions()
     for emoji in emojis:
         await message.add_reaction(str(emoji))
 
-    async def react(reaction , user):
-        global cur
+    async def react(reaction, user):
+        nonlocal cur
         if str(reaction.emoji) == "⏩":
-            if(cur == len(posts) - 1):
-                return
-            cur +=1
-            embed=discord.Embed(title=wrapped(posts[cur][0]), description="", color=242424 , url = posts[cur][1])
-            embed.set_footer(text=str(cur+1)+"/"+str(len(posts)))
+            if cur == len(posts) - 1:
+                return False
+            cur += 1
+            embed = discord.Embed(title=wrapped(posts[cur][0]), description="", color=242424, url=posts[cur][1])
+            embed.set_footer(text=str(cur + 1) + "/" + str(len(posts)))
             embed.set_image(url=posts[cur][1])
-            await message.edit(embed = embed)
+            await message.edit(embed=embed)
             await message.remove_reaction(reaction, user)
-                
+
         if str(reaction.emoji) == "⏪":
-            if(cur == 0):
-                return
-            cur -=1
-            embed=discord.Embed(title=wrapped(posts[cur][0]), description="", color=242424 , url = posts[cur][1])
-            embed.set_footer(text=str(cur+1)+"/"+str(len(posts)))
+            if cur == 0:
+                return False
+            cur -= 1
+            embed = discord.Embed(title=wrapped(posts[cur][0]), description="", color=242424, url=posts[cur][1])
+            embed.set_footer(text=str(cur + 1) + "/" + str(len(posts)))
             embed.set_image(url=posts[cur][1])
-            await message.edit(embed = embed)
+            await message.edit(embed=embed)
             await message.remove_reaction(reaction, user)
-            
+
         if str(reaction.emoji) == "🗑":
             await message.delete()
             await ctx.message.delete()
-            return
-    
-    while(True):
+            return True
+
+    while True:
         try:
             reaction, user = await bot.wait_for("reaction_add", timeout=10, check=check)
-            if(await Check(reaction , user)):
-                await react(reaction , user)
+            if await Check(reaction, user):
+                if await react(reaction, user):
+                    break
         except:
             await message.clear_reactions()
-            return;
+            break
+
 
 @album.error
-async def album_error_handler(ctx , error):
+async def album_error_handler(ctx, error):
     await ctx.send(error)
 
 
@@ -161,7 +172,7 @@ def time_format(seconds):
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
     return days, hours, minutes, seconds
-    
+
 
 def pretty_time_format(seconds, *, shorten=False, only_most_significant=False, always_seconds=False):
     days, hours, minutes, seconds = time_format(seconds)
@@ -182,8 +193,10 @@ def pretty_time_format(seconds, *, shorten=False, only_most_significant=False, a
 
     return ' '.join(map(format_, timeprint))
 
+
 @bot.command(name='uptime', help="Prints bot uptime")
 async def uptime(ctx):
     await ctx.send('Montana has been running for ' + pretty_time_format(time.time() - starting_time))
+
 
 bot.run(TOKEN)

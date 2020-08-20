e45d40f8120124a91f87ffcd2e1b354757bfd5b3
Parsa Alizadeh <33874579+ParsaAlizadeh@users.noreply.github.com>
2020-08-20 14:44:33 +0430

    Fix pdf feature (#15)
    * Reformat bot.py
    
    * Fix dm pdf
    
    * Reformat util.py
    
    * Add part number to pdf file name

--- libs/util.py
+++ libs/util.py
@@ -6,7 +6,7 @@ import requests
 from PIL import Image
 import os
 
-Image.MAX_IMAGE_PIXELS = 10000000000000000000000000000000  
+Image.MAX_IMAGE_PIXELS = 10000000000000000000000000000000
 
 colors = [0, 1752220, 3066993, 3447003, 10181046, 15844367, 15105570, 15158332,
           9807270, 8359053, 3426654, 1146986, 2067276, 2123412, 7419530, 12745742,
@@ -118,38 +118,45 @@ async def pagify(bot, ctx, links, names):
             break
 
 
-cnt = 0;
+cnt = 0
 
 
-async def send_pdf(ctx, name , links):
-    name += str(random.randint(0 , 1000000000))
+async def send_pdf(ctx, name, links):
     loading = await ctx.send(file=discord.File('libs/files/loading.gif'))
-    images = []
-    size_sum = 0;
     global cnt
-    while(cnt >= 2):
+    while cnt >= 2:
         await asyncio.sleep(2)
-    cnt+=1
+    cnt += 1
+
+    name += str(random.randint(0, 1000000000))
+    images = []
+    size_sum = 0
+    part_num = 1
+
     for link in links:
-            response = requests.head(link, allow_redirects=True)
-            size = int(response.headers.get('content-length', -1))
-            if(size < 2250000):
-                size_sum+=size;
-                images += [Image.open(requests.get(link, stream=True).raw).convert('RGB')]
-                if(size_sum > 4500000):
-                    images[0].save(name + '.pdf' ,save_all=True, append_images=images[1:])
-                    await ctx.send(file=discord.File(name + '.pdf'))
-                    for i in images:
-                        i.close()
-                    images = []
-                    os.remove(name+".pdf")
-                    size_sum = 0
-    if(len(images) > 0):
-        images[0].save(name + '.pdf' ,save_all=True, append_images=images[1:])
-        await ctx.send(file=discord.File(name + '.pdf'))
+        response = requests.head(link, allow_redirects=True)
+        size = int(response.headers.get('content-length', -1))
+        if size < 2250000:
+            size_sum += size
+            images.append(Image.open(requests.get(link, stream=True).raw).convert('RGB'))
+            if size_sum > 4500000:
+                filename = f'{name}_{part_num}.pdf'
+                images[0].save(filename, save_all=True, append_images=images[1:])
+                await ctx.send(file=discord.File(filename))
+                for i in images:
+                    i.close()
+                images.clear()
+                os.remove(filename)
+                size_sum = 0
+                part_num += 1
+
+    if len(images) > 0:
+        filename = f'{name}_{part_num}.pdf'
+        images[0].save(filename, save_all=True, append_images=images[1:])
+        await ctx.send(file=discord.File(filename))
         for i in images:
             i.close()
-        images = []
-        os.remove(name+".pdf")
+        os.remove(filename)
+
     await loading.delete()
     cnt -= 1

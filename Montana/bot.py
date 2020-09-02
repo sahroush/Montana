49a48a9a4e1d206ab2a784a300a26074dfe36818
soroush sahraei <55985277+sahroush@users.noreply.github.com>
2020-09-02 11:48:59 +0430

    Add files via upload

---
+++ Montana/bot.py
@@ -0,0 +1,140 @@
+import time
+from discord import Status
+from discord.ext import commands
+from libs.reddit import *
+from libs.util import *
+from libs.nhentai import *
+
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    TOKEN = "Njk5NTk5MzkwNDMxNzcyNzMz" + ".XpWutA.0nvimxpX" + "AW7uNlwRD" + "wW1aok8Zvw"  # The most idiotic idea
+
+bot = commands.Bot(command_prefix='`')
+STATUS = Status.online
+starting_time = time.time()
+
+
+@bot.event
+async def on_ready():
+    global starting_time
+    starting_time = time.time()
+    await bot.change_presence(activity=discord.Game(name="Use `help!"))
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to our Discord server!')
+
+
+@bot.command(name='echo', help='Repeats a given message', usage="[message...]")
+async def echo(ctx, *response):
+    if not response:
+        response = ["**I can't send an empty message you fucking idiot**"]
+    await ctx.send(" ".join(response))
+
+
+@bot.command(name='album', help='posts the most recent pics from the given subreddit \n'
+                                'nsfw is off in sfw channels unless +nsfw is used \n'
+                                'shuffles posts when +random is used'
+                                'sends a pdf instead of an album when +pdf is used',
+             usage="<subreddit> [+nsfw][+random][+pdf]")
+async def album(ctx, sub, *args):
+    sfw, nsfw = fetch(sub, "+pdf" in args)  # pdf ==> no gifs
+    posts = sfw
+    if ctx.channel.type is discord.ChannelType.private and "+pdf" not in args:
+        response = "Sorry, this command is not available in DMs :sob:"
+        await ctx.send(response)
+        return
+    if "+nsfw" in args or (ctx.channel.type is not discord.ChannelType.private and ctx.channel.is_nsfw()):
+        posts += nsfw
+    if not posts:
+        response = "Sorry, couldn't find a pic :sob:"
+        await ctx.send(response)
+        return
+    if "+random" in args:
+        random.shuffle(posts)
+    links = []
+    names = []
+    for i in posts:
+        links += [i[1]]
+        names += [i[0]]
+    if "+pdf" in args:
+        await send_pdf(ctx, sub, links)
+    else:
+        await pagify(bot, ctx, links, names)
+
+
+@bot.command(name='nhentai',
+             help='posts the given sauce \n' +
+                  'nsfw is off in sfw channels unless +nsfw is used \n' +
+                  'sends a pdf instead of an album when +pdf is used',
+             usage="<source number> [+nsfw][+pdf]" , hidden = True)
+async def nhentai(ctx, sixdigit: int, *args):
+    posts, name = fetch_hentai(sixdigit)
+    if ctx.channel.type is discord.ChannelType.private and "+pdf" not in args:
+        response = "Sorry, this command is not available in DMs :sob:"
+        await ctx.send(response)
+        return
+    if not ("+nsfw" in args or ctx.channel.is_nsfw()):
+        response = "Sorry, this commnd will only work either when used in a nsfw channel or with +nsfw tag used"
+        await ctx.send(response)
+        return
+    if not posts:
+        response = "Sorry, couldn't find a Doujin :sob:"
+        await ctx.send(response)
+        return
+    names = [name] * len(posts)
+    if "+pdf" in args:
+        await send_pdf(ctx, name, posts)
+    else:
+        await pagify(bot, ctx, posts, names)
+
+
+@bot.command(name='ping', help="Used to test Montana's response time.")
+async def ping(ctx):
+    start = time.perf_counter()
+    message = await ctx.send(':ping_pong: Pong!')
+    end = time.perf_counter()
+    duration = (end - start) * 1000
+    await message.edit(content=f'REST API latency: {int(duration)}ms\n'
+                               f'Gateway API latency: {int(bot.latency * 1000)}ms')
+
+
+@bot.command(name='uptime', help="Prints bot uptime")
+async def uptime(ctx):
+    global starting_time
+    await ctx.send('Montana has been running for ' + pretty_time_format(time.time() - starting_time))
+
+
+@bot.command(name="dokme", help="Toggle status", aliases=['lurk'] , hidden = True)
+@commands.has_role('Admin')
+async def dokme(ctx):
+    global STATUS
+    if STATUS is Status.invisible:
+        STATUS = Status.online
+        await ctx.send(embed=make_embed("I am online now"))
+    elif STATUS is Status.online:
+        STATUS = Status.invisible
+        await ctx.send(embed=make_embed("Pushed dokme successfully"))
+    await bot.change_presence(status=STATUS, activity=discord.Game(name="Use `help!"))
+    
+    
+@bot.command(name='makepdf', help='converts a webpage into a pdf \n' , usage="<url>")
+async def convert2pdf(ctx, url, *args):
+    try:
+        response = requests.get(url)
+        await site2pdf(ctx , url)
+    except requests.ConnectionError as exception:
+        raise("URL does not exist on Internet")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if STATUS is Status.invisible:
+        return
+    await ctx.send(embed=make_embed(error))
+
+
+bot.run(TOKEN)
---
+++ Montana/libs/util.py
@@ -0,0 +1,225 @@
+import textwrap
+import discord
+import asyncio
+import random
+import requests
+import img2pdf
+import os
+from PIL import Image #cuz alpha is a bitch
+import urllib.parse
+import weasyprint
+
+
+colors = [0, 1752220, 3066993, 3447003, 10181046, 15844367, 15105570, 15158332,
+          9807270, 8359053, 3426654, 1146986, 2067276, 2123412, 7419530, 12745742,
+          11027200, 10038562, 9936031, 12370112, 2899536, 16580705, 12320855]
+
+
+def wrapped(s):
+    wrapper = textwrap.TextWrapper(width=20)
+    word_list = wrapper.wrap(text=s)
+    s = ""
+    for word in word_list:
+        s += '\n' + word
+    return s
+
+
+def time_format(seconds):
+    seconds = int(seconds)
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    return days, hours, minutes, seconds
+
+
+def pretty_time_format(seconds, *, shorten=False, only_most_significant=False, always_seconds=False):
+    days, hours, minutes, seconds = time_format(seconds)
+    timespec = [
+        (days, 'day', 'days'),
+        (hours, 'hour', 'hours'),
+        (minutes, 'minute', 'minutes'),
+    ]
+    timeprint = [(cnt, singular, plural) for cnt, singular, plural in timespec if cnt]
+    if not timeprint or always_seconds:
+        timeprint.append((seconds, 'second', 'seconds'))
+    if only_most_significant:
+        timeprint = [timeprint[0]]
+
+    def format_(triple):
+        cnt, singular, plural = triple
+        return f'{cnt}{singular[0]}' if shorten else f'{cnt} {singular if cnt == 1 else plural}'
+
+    return ' '.join(map(format_, timeprint))
+
+
+def make_embed(text):
+    return discord.Embed(description=str(text), color=colors[random.randint(0, len(colors) - 1)])
+
+
+async def pagify(bot, ctx, links, names):
+    cur = 0
+    embed = discord.Embed(title=wrapped(names[cur]), description="", color=colors[random.randint(0, len(colors) - 1)],
+                          url=links[cur])
+    embed.set_footer(text=str(cur + 1) + "/" + str(len(links)))
+    embed.set_image(url=links[cur])
+
+    message = await ctx.send(embed=embed)
+
+    emojis = ["⏪", "⏩", "🗑"]
+
+    def check(reaction, user):
+        if user == message.author or reaction.message.id != message.id:  # the reaction was made by the bot itself
+            return False
+        return True  # made by user || third party
+
+    async def Check(reaction, user):
+        await message.remove_reaction(reaction, user)
+        return not (user != ctx.author or not (str(reaction) in emojis))
+
+    await message.clear_reactions()
+    for emoji in emojis:
+        await message.add_reaction(str(emoji))
+
+    async def react(reaction, user):
+        nonlocal cur
+        if str(reaction.emoji) == "⏩":
+            if cur == len(links) - 1:
+                return False
+            cur += 1
+            embed = discord.Embed(title=wrapped(names[cur]), description="", color=242424, url=links[cur])
+            embed.set_footer(text=str(cur + 1) + "/" + str(len(links)))
+            embed.set_image(url=links[cur])
+            await message.edit(embed=embed)
+            await message.remove_reaction(reaction, user)
+
+        if str(reaction.emoji) == "⏪":
+            if cur == 0:
+                return False
+            cur -= 1
+            embed = discord.Embed(title=wrapped(names[cur]), description="", color=242424, url=links[cur])
+            embed.set_footer(text=str(cur + 1) + "/" + str(len(links)))
+            embed.set_image(url=links[cur])
+            await message.edit(embed=embed)
+            await message.remove_reaction(reaction, user)
+
+        if str(reaction.emoji) == "🗑":
+            await message.delete()
+            await ctx.message.delete()
+            return True
+
+    while True:
+        try:
+            reaction, user = await bot.wait_for("reaction_add", timeout=180, check=check)
+            if await Check(reaction, user):
+                if await react(reaction, user):
+                    break
+        except asyncio.TimeoutError:
+            await message.clear_reactions()
+            break
+        except:
+            break
+
+
+
+
+async def upload(name):
+    best_server = requests.get('https://apiv2.gofile.io/getServer').json()
+    server = best_server['data']['server']
+    files = {
+        'file': (name, open(name, 'rb') , "application/pdf")
+    }
+    response = requests.post('https://'+server+\
+     '.gofile.io/uploadFile', files=files).json()['data']['code']
+    return("https://gofile.io/?c="+response)
+
+
+cnt = 0
+
+
+async def makepdf(links , name): # low memory usage but slow af
+    images = []
+    img_num = 1
+    for link in links:
+        response = requests.head(link, allow_redirects=True)
+        size = int(response.headers.get('content-length', -1))
+        if size < 5000000:
+            img = open(name + str(img_num) + ".wtf" , "wb")
+            img.write(requests.get(link).content)
+            img.close()
+            images.append(name + str(img_num) + ".wtf")
+            img_num+=1
+    filename = f'{name}_{img_num}.pdf'
+    pdf = open(filename , "wb")
+    pdf.write(img2pdf.convert(images))
+    pdf.close()
+    for i in images:
+        os.remove(i)
+    return(filename)
+    
+async def fastmakepdf(links , name): # super high memory usage but fast
+    images = []
+    for link in links:
+        response = requests.head(link, allow_redirects=True)
+        size = int(response.headers.get('content-length', -1))
+        if size < 5000000:
+            images.append(Image.open(requests.get(link, stream=True).raw).convert('RGB'))
+    filename = f'{name}.pdf'
+    images[0].save(filename, save_all=True, append_images=images[1:])
+    for i in images:
+        i.close()
+    return(filename)
+    
+async def send_pdf(ctx, name, links):
+    if(len(name) > 25):
+          name = name[:20]
+    originalname = name
+    loading = await ctx.send(file=discord.File('libs/files/loading.gif'))
+    global cnt
+    while cnt >= 9:
+        await asyncio.sleep(2)
+    cnt += 1
+    name += str(random.randint(0, 1000000000))
+    if(len(links) > 50 ) :
+        filename = await makepdf(links , name)
+    else :
+        filename = await fastmakepdf(links , name)
+    url = await upload(filename)
+    embed = discord.Embed(title=originalname, description="", color=colors[random.randint(0, len(colors) - 1)],
+                          url=url)
+    await ctx.send(embed = embed)
+    os.remove(filename)
+    await loading.delete()
+    cnt -= 1
+
+def extract_domain(url, remove_http=True):
+    uri = urllib.parse.urlparse(url)
+    if remove_http:
+        domain_name = f"{uri.netloc}"
+    else:
+        domain_name = f"{uri.netloc}://{uri.netloc}"
+    return domain_name
+
+async def site2pdf(ctx , url):
+    pdf = weasyprint.HTML(url).write_pdf()
+    originalname = extract_domain(url);
+    filename = extract_domain(url);
+    if(len(filename) > 10):
+        filename = filename[:10]
+    filename+= str(random.randint(1 , 10**9)) + ".pdf"
+    pdf_file = open(filename, 'wb')
+    pdf_file.write(pdf)
+    pdf_file.close()
+    loading = await ctx.send(file=discord.File('libs/files/loading.gif'))
+    global cnt
+    while cnt >= 9:
+        await asyncio.sleep(2)
+    cnt += 1
+    url = await upload(filename)
+    embed = discord.Embed(title=originalname, description="", color=colors[random.randint(0, len(colors) - 1)],
+                          url=url)
+    await ctx.send(embed = embed)
+    os.remove(filename)
+    await loading.delete()
+    cnt -= 1
+    
+    

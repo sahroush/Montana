e91d80579b7600633055918eb5e816b3dd61d7ad
soroush sahraei <55985277+sahroush@users.noreply.github.com>
2020-10-02 19:52:09 +0330

    Update reddit.py

--- libs/reddit.py
+++ libs/reddit.py
@@ -5,32 +5,24 @@ def makeUrl(afterID, sub):
     return sub.split('/.json')[0] + "/.json?after={}".format(afterID)
 
 
-def ismedia(imageUrl, pdf):
+def ismedia(imageUrl):
     ismed = ('.jpg' in imageUrl or
              '.webm' in imageUrl or
              ('.gif' in imageUrl and not pdf) or
              '.png' in imageUrl) and '.gifv' not in imageUrl
-    if pdf:
-        response = requests.head(imageUrl, allow_redirects=True)
-        size = int(response.headers.get('content-length', -1))
-        if size > 2250000:
-            ismed = 0
     return ismed
 
 
-def fetch(sub, pdf):
+def fetch(sub):
     url = makeUrl('', "https://www.reddit.com/r/" + sub)
-    subJson = requests.get(url, headers={'User-Agent': 'Montana'}).json()
+    subJson = requests.get(url, headers={'User-Agent': 'NekoBot'}).json()
     sfw = []
-    nsfw = []
     try:
         posts = subJson['data']['children']
         for post in posts:
-            ismed = ismedia(post['data']['url'], pdf)
-            if ismed and post['data']['over_18']:
-                nsfw += [[post['data']['title'], post['data']['url']]]
-            elif ismed:
+            ismed = ismedia(post['data']['url'])
+            if ismed:
                 sfw += [[post['data']['title'], post['data']['url']]]
     except:
         pass
-    return sfw, nsfw
+    return sfw

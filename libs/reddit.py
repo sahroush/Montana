6e3a862509a89a17b64583ec4e7458c8b823cab2
ParsaAlizadeh <parsa.alizadeh1@gmail.com>
2021-07-02 19:07:11 +0430

    Use async libraries

--- libs/reddit.py
+++ libs/reddit.py
@@ -1,4 +1,5 @@
-import requests
+import aiohttp
+from libs.util import with_session
 
 
 def makeUrl(afterID, sub):
@@ -10,27 +11,36 @@ def ismedia(imageUrl, pdf):
              '.webm' in imageUrl or
              ('.gif' in imageUrl and not pdf) or
              '.png' in imageUrl) and '.gifv' not in imageUrl
-    if pdf:
-        response = requests.head(imageUrl, allow_redirects=True)
-        size = int(response.headers.get('content-length', -1))
-        if size > 2250000:
-            ismed = 0
     return ismed
 
 
-def fetch(sub, pdf):
+@with_session
+async def fetch(session, sub, pdf):
     url = makeUrl('', "https://www.reddit.com/r/" + sub)
-    subJson = requests.get(url, headers={'User-Agent': 'Montana'}).json()
+    async with session.get(url) as resp:
+        sub_json = await resp.json()
     sfw = []
     nsfw = []
-    try:
-        posts = subJson['data']['children']
-        for post in posts:
-            ismed = ismedia(post['data']['url'], pdf)
-            if ismed and post['data']['over_18']:
-                nsfw += [[post['data']['title'], post['data']['url']]]
-            elif ismed:
-                sfw += [[post['data']['title'], post['data']['url']]]
-    except:
-        pass
+    posts = sub_json['data']['children']
+    for post in posts:
+        if 'title' not in post['data'] or 'url' not in post['data']:
+            continue
+        title, url = post['data']['title'], post['data']['url']
+
+        # check file type
+        if not ismedia(url, pdf):
+            continue
+
+        # check file size
+        if pdf:
+            async with session.head(url, allow_redirects=True) as resp:
+                size = int(resp.headers.get('Content-Length', -1))
+            if size > 2.25e6:   # 2.25 MB
+                continue
+
+        # add to their category
+        if post['data']['over_18']:
+            nsfw.append((title, url))
+        else:
+            sfw.append((title, url))
     return sfw, nsfw

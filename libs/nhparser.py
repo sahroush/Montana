51946fa84a0f7e29fa31adae6e18425626b919f6
soroush sahraei <soroush.sahraei@gmail.com>
2022-07-24 11:46:47 +0430

    Delete nhparser.py

--- libs/nhparser.py
+++
@@ -1,9 +0,0 @@
-import nhentai as nh
-
-
-def fetch_hentai(sixdigit):
-    douj = nh.Doujinshi(sixdigit)
-    links = []
-    for i in range(douj.pages):
-        links.append(douj[i])
-    return links, douj.name
